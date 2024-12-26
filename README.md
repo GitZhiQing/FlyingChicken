a4068e44bc02f8089a443edb6af0a807b1e70d93
QING <qlear@qq.com>
2024-12-27 01:08:18 +0800

    refactor: 通过环境变量初始化数据库
    - docs: 完善 README
    - build: 完善 .dockerignore, docker-compose.yml

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Flying Chicken
 
 一个简单的签到 Web 应用，基于 Flask 框架，使用 SQLite 数据库。
+
+## 部署
+
+建议在 Linux 系统下部署。
+
+```bash
+git clone https://github.com/GitZhiQing/FlyingChicken.git && cd FlyingChicken
+# 设置 docker-compose.yml 中的环境变量，然后
+docker-compose up -d
+```
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -23,3 +25,22 @@ app.add_template_filter(utc_timestamp_to_shanghai_datetime)
 
 
 from app import routes, errors, commands  # noqa
+
+
+@app.before_request
+def initialize_database():
+    """初始化数据库"""
+    from app.models import Admins
+
+    if not os.path.exists(os.path.join(app.instance_path, "data.db")):
+        db.create_all()
+        admin_name = os.getenv("ADMIN_NAME")
+        admin_password = os.getenv("ADMIN_PASSWORD")
+        if admin_name and admin_password:
+            admin_user = Admins(username=admin_name)
+            admin_user.set_password_hash(admin_password)
+            db.session.add(admin_user)
+            db.session.commit()
+            logging.info("管理员账户创建完成.")
+        else:
+            logging.warning("未设置管理员账户.")
