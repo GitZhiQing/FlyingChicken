9d1514bca447c732c1cec9a553995b6458b52057
QING <qlear@qq.com>
2024-12-27 09:20:52 +0800

    fix: 再次解决时区不一致导致的显示时间问题

--- app/filters.py
+++ app/filters.py
@@ -1,7 +1,10 @@
-import time
+import pytz
+from datetime import datetime
 
 
 def utc_timestamp_to_shanghai_datetime(utc_timestamp):
     """UTC 时间戳转换成上海时间"""
-    timeArray = time.localtime(utc_timestamp)
-    return time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    utc_dt = datetime.fromtimestamp(utc_timestamp, pytz.utc)
+    shanghai_tz = pytz.timezone("Asia/Shanghai")
+    shanghai_dt = utc_dt.astimezone(shanghai_tz)
+    return shanghai_dt.strftime("%Y-%m-%d %H:%M:%S")
